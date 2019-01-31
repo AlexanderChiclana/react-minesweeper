bac039fe98d421134e825902a05552f11fa9a67c
AlexanderChiclana <alexander_chiclana@alumni.brown.edu>
2019-01-31 13:25:10 -0500

    got random placement working

--- src/components/gameboard.js
+++ src/components/gameboard.js
@@ -2,14 +2,36 @@ import React, { Component } from 'react'
 import Space from './space'
 
 class Gameboard extends Component {
-    state = {  }
+    state = { 
+      xGrid: 0,
+      yGrid: 0,
 
+      boardArray: []
+    }
 
+    // reveal needs to happen at this level? 
 
     render() { 
+
+
+      // const boardArray = []
+      for (let i = 0; i < 64; i++) {
+        let bomb = false
+        if (Math.floor(Math.random() * 11) > 8 ) {
+          
+          bomb = true
+        }
+
+        this.state.boardArray.push(
+          <Space key={i} bomb={bomb}/>
+        )
+      }
+
       return ( <div className='gameBoard'>
 
-        <Space bomb={true}/> 
+        {this.state.boardArray}
+
+        {/* <Space bomb={true}/> 
         <Space /> 
         <Space bomb={true}/> 
         <Space /> 
@@ -48,7 +70,7 @@ class Gameboard extends Component {
         <Space /> 
         <Space /> 
         <Space /> 
-        <Space /> 
+        <Space />  */}
 
       </div> )
     }

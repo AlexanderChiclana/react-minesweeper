8b9a39a183d13aef7a67c175dc976688ee0b4d91
AlexanderChiclana <alexander_chiclana@alumni.brown.edu>
2019-01-31 16:20:18 -0500

    got working logic for checking and summing

--- src/components/gameboard.js
+++ src/components/gameboard.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Space from './space'
 
+
 class Gameboard extends Component {
     state = { 
       xGrid: 0,
@@ -14,18 +15,7 @@ class Gameboard extends Component {
     render() { 
 
 
-      // const boardArray = []
-      // for (let i = 0; i < 64; i++) {
-      //   let bomb = false
-      //   if (Math.floor(Math.random() * 11) > 8 ) {
-      //     bomb = true
-      //   }
-
-      //   this.state.boardArray.push(
-      //     <Space key={i} bomb={bomb}/>
-      //   )
-      // }
-
+     
       for (let i = 0; i < 8; i++) {
         this.state.boardArray.push([])
         for (let j = 0; j < 8; j++) {
@@ -36,6 +26,34 @@ class Gameboard extends Component {
           this.state.boardArray[i].push(
             <Space key={i.toString() + j.toString()} bomb={bomb}/>
           )
+        }
+      }
+
+      // seperate function to assign adjacent Number prop. Needs to iterate through 2d array, and perform logic check to count of adjacent slots with bomb
+      console.log(this.state.boardArray[1][1].props.bomb)
+      
+      
+
+      for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+          // console.log(i.toString() + j.toString())
+          // loops through each index of the 2d array. Need to sum all adjacent slots THEN pass count as prop 
+          const boardIndex = this.state.boardArray
+          let adjSum = 0
+
+          try{
+            if (
+              boardIndex[i][j + 1].props.bomb == true) {
+              adjSum++
+              console.log('to the right of ',i.toString() + j.toString() , 'is a bomb')
+            }
+          }catch(error){
+          }
+          // need to do this for every adj combo
+
+          
+
+          // console.log(boardIndex[i][j].props.bomb)
         }
       }
 

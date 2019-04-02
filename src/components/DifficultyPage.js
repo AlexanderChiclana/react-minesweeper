75da69a3f59ab6729fbb67c022f5e37418f5a693
AlexanderChiclana <alexander_chiclana@alumni.brown.edu>
2019-04-02 18:54:50 -0400

    building UI

--- src/components/DifficultyPage.js
+++ src/components/DifficultyPage.js
@@ -9,6 +9,7 @@ import Typography from '@material-ui/core/Typography'
 import Grid from '@material-ui/core/Grid'
 import Fab from '@material-ui/core/Fab'
 import NavigationIcon from '@material-ui/icons/Navigation'
+import { Link } from 'react-router-dom'
 
 
 
@@ -32,19 +33,40 @@ const styles = {
     marginBottom: 12,
   },
   margin: {
-    margin: theme.spacing.unit,
+    // margin: theme.spacing.unit,
   },
   extendedIcon: {
-    marginRight: theme.spacing.unit,
+    outline: 0,
+    // marginRight: theme.spacing.unit,
   },
 }
 
 function SimpleCard(props) {
   const { classes } = props
-  const bull = <span className={classes.bullet}>•</span>
 
   return (
     <div className={classes.root}>
+
+<Grid container
+  spacing={0}
+  direction="column"
+  alignItems="center"
+  justify="center">
+
+    <Grid item xs={12} sm={12} lg={12} xl={12}   alignItems="center" alignContent="center">
+
+    <Typography component="h2" variant="h1" gutterBottom>
+    React Minesweeper
+    </Typography>
+    </Grid>
+    <Grid item xs={12} sm={12} lg={12} xl={12}>
+    <Typography variant="h5" gutterBottom>
+
+    Choose a difficulty, or create your own
+    </Typography>
+    </Grid>
+</Grid>
+
 
     <Grid container spacing={8}>
 
@@ -68,7 +90,14 @@ function SimpleCard(props) {
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">Play</Button>
+
+        <Link to='/easy'>
+        <Fab variant="extended" color="primary" aria-label="Add" className={classes.margin}>
+          <NavigationIcon className={classes.extendedIcon} />
+          Play Beginner
+        </Fab>  
+        </Link>
+
       </CardActions>
     </Card>
     </Grid>
@@ -92,8 +121,15 @@ function SimpleCard(props) {
     </Typography>
   </CardContent>
   <CardActions>
-    <Button size="small">Play</Button>
-  </CardActions>
+
+     <Link to='/medium'>
+         <Fab variant="extended" color="primary" aria-label="Add" className={classes.margin}>
+          <NavigationIcon className={classes.extendedIcon} />
+          Play Intermediate
+        </Fab>  
+    </Link>
+
+   </CardActions>
 </Card>
 </Grid>
 
@@ -117,13 +153,46 @@ function SimpleCard(props) {
   </CardContent>
   <CardActions>
 
-  <Fab variant="extended" color="primary" aria-label="Add" className={classes.margin}>
+    <Link to='/hard'>
+        <Fab variant="extended" color="primary" aria-label="Add" className={classes.margin}>
           <NavigationIcon className={classes.extendedIcon} />
-          Extended
-        </Fab>  </CardActions>
+          Play Expert
+        </Fab>  
+    </Link>
+
+    </CardActions>
 </Card>
 </Grid>
 
+
+<Grid item xs={12} sm={6} lg={4} xl={4}>
+<Card className={classes.card}>
+  <CardContent>
+    <Typography className={classes.title} color="textSecondary" gutterBottom>
+      Difficulty
+    </Typography>
+    <Typography variant="h5" component="h2">
+       Custom
+    </Typography>
+    <Typography className={classes.pos} color="textSecondary">
+      adjective
+    </Typography>
+    <Typography component="p">
+      well meaning and kindly.
+      <br />
+      {'"a benevolent smile"'}
+    </Typography>
+  </CardContent>
+  <CardActions>
+
+        <Fab variant="extended" color="primary" aria-label="Add" className={classes.margin}>
+          <NavigationIcon className={classes.extendedIcon} />
+            Create Custom Game
+        </Fab>  
+
+    </CardActions>
+</Card>
+</Grid>
     </Grid>
     </div>
   )
